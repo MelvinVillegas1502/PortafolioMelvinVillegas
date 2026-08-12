db59832590332273011c1cea0319bc9db939fd88
Melvin420 <mvillegas60790@ufide.ac.cr>
2026-08-12 10:24:21 -0600

    Semana 10 Usuarios y  Roles desde BD

--- TechShop/src/main/java/com/TechShop/SecurityConfig.java
+++ TechShop/src/main/java/com/TechShop/SecurityConfig.java
@@ -1,14 +1,16 @@
 package com.TechShop;
 
+import com.TechShop.domain.Ruta;
+import com.TechShop.service.RutaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -22,56 +24,31 @@ public class SecurityConfig implements WebMvcConfigurer {
         registry.addViewController("/acceso_denegado").setViewName("acceso_denegado");
     }
 
-    public static final String[] PUBLIC_URLS = {
-        "/",
-        "/index",
-        "/login",
-        "/acceso_denegado",
-        "/css/**",
-        "/js/**",
-        "/img/**",
-        "/images/**",
-        "/webjars/**",
-        "/favicon.ico",
-        "/manifest.json",
-        "/consultas/**"
-    };
-
-    public static final String[] ADMIN_URLS = {
-        "/producto/nuevo",
-        "/producto/guardar",
-        "/producto/modificar/**",
-        "/producto/eliminar/**",
-        "/categoria/nuevo",
-        "/categoria/guardar",
-        "/categoria/modificar/**",
-        "/categoria/eliminar/**",
-        "/usuario/nuevo",
-        "/usuario/guardar",
-        "/usuario/modificar/**",
-        "/usuario/eliminar/**"
-    };
-
-    public static final String[] ADMIN_OR_VENDEDOR_URLS = {
-        "/producto/listado",
-        "/categoria/listado",
-        "/usuario/listado"
-    };
-
-    public static final String[] USUARIO_URLS = {
-        "/carrito/**",
-        "/facturar/**"
-    };
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(request -> request
-                .requestMatchers(PUBLIC_URLS).permitAll()
-                .requestMatchers(ADMIN_URLS).hasRole("ADMIN")
-                .requestMatchers(ADMIN_OR_VENDEDOR_URLS).hasAnyRole("ADMIN", "VENDEDOR")
-                .requestMatchers(USUARIO_URLS).hasRole("USUARIO")
-                .anyRequest().authenticated()
-        ).formLogin(form -> form
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, @Lazy RutaService rutaService)
+            throws Exception {
+
+        var rutas = rutaService.getRutas();
+
+        http.authorizeHttpRequests(requests -> {
+
+            for (Ruta ruta : rutas) {
+
+                if (!ruta.isRequiereRol()) {
+                    requests.requestMatchers(ruta.getRuta()).permitAll();
+                } else if (ruta.getRol().getRol().equals("ADMIN")) {
+                    requests.requestMatchers(ruta.getRuta()).hasRole("ADMIN");
+                } else if (ruta.getRol().getRol().equals("VENDEDOR")) {
+                    requests.requestMatchers(ruta.getRuta()).hasAnyRole("ADMIN", "VENDEDOR");
+                } else if (ruta.getRol().getRol().equals("USUARIO")) {
+                    requests.requestMatchers(ruta.getRuta()).hasAnyRole("ADMIN", "USUARIO");
+                }
+            }
+
+            requests.anyRequest().authenticated();
+        });
+
+        http.formLogin(form -> form
                 .loginPage("/login")
                 .loginProcessingUrl("/login")
                 .defaultSuccessUrl("/", true)
@@ -85,6 +62,9 @@ public class SecurityConfig implements WebMvcConfigurer {
                 .permitAll()
         ).exceptionHandling(exceptions -> exceptions
                 .accessDeniedPage("/acceso_denegado")
+        ).sessionManagement(session -> session
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(false)
         );
 
         return http.build();
@@ -95,26 +75,11 @@ public class SecurityConfig implements WebMvcConfigurer {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public UserDetailsService users(PasswordEncoder passwordEncoder) {
-        UserDetails admin = User.builder()
-                .username("juan")
-                .password(passwordEncoder.encode("123"))
-                .roles("ADMIN")
-                .build();
+    @Autowired
+    public void configurerGlobal(AuthenticationManagerBuilder build,
+            @Lazy PasswordEncoder passwordEncoder,
+            @Lazy UserDetailsService userDetailsService) throws Exception {
 
-        UserDetails vendedor = User.builder()
-                .username("rebeca")
-                .password(passwordEncoder.encode("456"))
-                .roles("VENDEDOR")
-                .build();
-
-        UserDetails usuario = User.builder()
-                .username("pedro")
-                .password(passwordEncoder.encode("789"))
-                .roles("USUARIO")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, vendedor, usuario);
+        build.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 }
---
+++ TechShop/src/main/java/com/TechShop/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.TechShop.repository;
+
+import com.TechShop.domain.Usuario;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    public Optional<Usuario> findByUsernameAndActivoTrue(String username);
+}
